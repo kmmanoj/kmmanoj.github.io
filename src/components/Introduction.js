da23d3b3c08e03e3917ee284569179d435742163
Manoj Vignesh K M <kmmanojv96@gmail.com>
2024-02-25 19:11:57 -0800

    No web3

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -3,32 +3,7 @@ import { Container, Header, Message, Icon, Button } from "semantic-ui-react";
 
 import "semantic-ui-css/semantic.min.css";
 
-function getLastWeb3curiosity() {
-    let curious = localStorage.getItem("web3curiosity");
-    return !curious;
-}
-
-function setLastWeb3curiosity() {
-    localStorage.setItem("web3curiosity", false);
-}
-
 export default function Introduction() {
-    let [web3curiosity, setWeb3curiosity] = useState(getLastWeb3curiosity());
-
-    let web3curiosityMsg = (
-        <Message attached='bottom' info>
-            <Icon name='info' />
-            Curious how my portfolio in web3 feels like?
-            <Button color="blue" size="mini" style={{margin: "0px 10px", padding: "5px"}} onClick={() => {
-                window.location.href = "https://kmmanoj.github.io/web3"
-            }}>Yes, Take me there!</Button>
-            <Button color="red" size="mini" style={{margin: "0px 10px", padding: "5px"}}onClick={() => {
-                setWeb3curiosity(false);
-                setLastWeb3curiosity();
-            }}>Don't show this again!</Button>
-        </Message>
-    );
-    
     return (
         <div style={{"padding": "2% 0"}}>
             <Container text>
@@ -39,7 +14,6 @@ export default function Introduction() {
                 <p>
                     Check out my <a href="https://kmmanoj.hashnode.dev" target="_blank" rel="noreferrer">blog articles</a> where I explain claims and concepts by application.
                 </p>
-                {web3curiosity?web3curiosityMsg:<div></div>}
             </Container>
         </div>
     );
